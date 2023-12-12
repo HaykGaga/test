fd4065377baaed7ff5d56ad0049162f308391bd4
Hayk <hayk.inanc@gmail.com>
2023-12-12 20:46:29 +0400

    init commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+// 1) создать репозиторий
+// 2) добавить все файлы в staging
+// 3) сделать commit "init commit"
